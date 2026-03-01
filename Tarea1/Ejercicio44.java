d3fd9f7bf361ced6716657c4404aa0f6c1698ade
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-01 15:46:20 +0000

    Confirmar

---
+++ Tarea1/Ejercicio44.java
@@ -0,0 +1,24 @@
+package Tarea1;
+
+import java.util.Scanner;
+
+public class Ejercicio44 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+          
+        System.out.print("Ingrese un número: ");
+        double num = sc.nextDouble();
+        
+       
+        if (num < 0) {
+            System.out.println("No se puede calcular la raíz cuadrada de un número negativo.");
+        } else {
+            double raiz = Math.sqrt(num);
+            System.out.println("La raíz cuadrada de " + num + " es " + raiz);
+        }
+
+        sc.close();
+    }
+}
+
+
