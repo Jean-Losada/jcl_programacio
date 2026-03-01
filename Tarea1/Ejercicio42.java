4027b53ced50009b9257aa38a7b6245fb25df9f8
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-01 16:03:12 +0000

    Punto42ok

--- Tarea1/Ejercicio42.java
+++ Tarea1/Ejercicio42.java
@@ -6,13 +6,11 @@ public class Ejercicio42 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-       
         System.out.print("Ingrese el primer número: ");
         double num1 = sc.nextDouble();
         System.out.print("Ingrese el segundo número: ");
         double num2 = sc.nextDouble();
 
-        
         if (num1 > num2) {
             System.out.println("El primer número es el mayor.");
         } else if (num1 < num2) {
