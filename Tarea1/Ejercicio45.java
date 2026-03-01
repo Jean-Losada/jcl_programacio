bc2ac4b824ffc88791757f819f9d45752727ce0d
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-01 15:59:55 +0000

    Punto45ok

--- Tarea1/Ejercicio45.java
+++ Tarea1/Ejercicio45.java
@@ -6,11 +6,9 @@ public class Ejercicio45 {
     public static void main(String[] args) {
          Scanner sc = new Scanner(System.in);
 
-        // --- ENTRADA ---
         System.out.print("Ingrese un número: ");
         int num = sc.nextInt();
 
-        // --- PROCESO Y SALIDA ---
         if (num % 2 == 0) {
             System.out.println("El número es par.");
         } else {
