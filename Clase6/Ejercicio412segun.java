1d1df806967fe922247c35a591fcd872e0a82c4d
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-09 17:19:04 +0000

    version14ok

--- Clase6/Ejercicio412segun.java
+++ Clase6/Ejercicio412segun.java
@@ -4,12 +4,12 @@ import java.util.Scanner;
 
 public class Ejercicio412segun {
     public static void main(String[] args) {
-         Scanner sc = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
 
-        double num1,num2;
+        double num1, num2;
         int seleccion;
 
-        System.out.println( "Digite el numero 1");
+        System.out.println("Digite el numero 1");
         num1 = sc.nextDouble();
         System.out.println("Digite el numero 2");
         num2 = sc.nextDouble();
@@ -17,20 +17,13 @@ public class Ejercicio412segun {
         seleccion = sc.nextInt();
 
         switch (seleccion) {
-            case 1:
-                System.out.println("la suma es:" + (num1+num2));
-                break;
-            case 2:
-                System.out.println("la multiplicacion es:" + (num1*num2));
-                break;
-            case 3:
-                System.out.println("la divison es:" + (num1/num2));
-                break;
-            default:
-                System.out.println("Opcion no valida");
-                break;
+            case 1 -> System.out.println("la suma es:" + (num1 + num2));
+            case 2 -> System.out.println("la multiplicacion es:" + (num1 * num2));
+            case 3 -> System.out.println("la divison es:" + (num1 / num2));
+            default -> System.out.println("Opcion no valida");
+
         }
         sc.close();
     }
-    
+
 }
