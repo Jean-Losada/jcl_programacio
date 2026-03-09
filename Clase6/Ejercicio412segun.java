7155a64f497023bbe4858b03f2d2e8697380decc
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-09 16:49:53 +0000

    Ejercicio412segunok

---
+++ Clase6/Ejercicio412segun.java
@@ -0,0 +1,36 @@
+package Clase6;
+
+import java.util.Scanner;
+
+public class Ejercicio412segun {
+    public static void main(String[] args) {
+         Scanner sc = new Scanner(System.in);
+
+        double num1,num2;
+        int seleccion;
+
+        System.out.println( "Digite el numero 1");
+        num1 = sc.nextDouble();
+        System.out.println("Digite el numero 2");
+        num2 = sc.nextDouble();
+        System.out.println("Digite la seleccion 1, 2 o 3");
+        seleccion = sc.nextInt();
+
+        switch (seleccion) {
+            case 1:
+                System.out.println("la suma es:" + (num1+num2));
+                break;
+            case 2:
+                System.out.println("la multiplicacion es:" + (num1*num2));
+                break;
+            case 3:
+                System.out.println("la divison es:" + (num1/num2));
+                break;
+            default:
+                System.out.println("Opcion no valida");
+                break;
+        }
+        sc.close();
+    }
+    
+}
