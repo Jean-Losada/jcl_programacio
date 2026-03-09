1894af7716c231b7bd38286dd426ae9de4748fe7
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-09 16:01:47 +0000

    412ok

--- Clase6/Ejercicio412.java
+++ Clase6/Ejercicio412.java
@@ -10,20 +10,20 @@ public class Ejercicio412 {
         int seleccion;
 
         System.out.println( "Digite el numero 1");
-        num1 = sc.nextInt();
+        num1 = sc.nextDouble();
         System.out.println("Digite el numero 2");
-        num2 = sc.nextInt();
-        System.out.println("Digite la seleccion 1,2 o 3");
+        num2 = sc.nextDouble();
+        System.out.println("Digite la seleccion 1, 2 o 3");
         seleccion = sc.nextInt();
 
-        if (seleccion==1){
-            System.out.println("la suma"+(num1+num2));
+        if(seleccion==1){
+            System.out.println("la suma" + (num1 + num2));
         }else{
             if (seleccion==2){
-                System.out.println("la multiplicacion"+(num1*num2));
+                System.out.println("la multiplicacion"+(num1 * num2));
             }else{
                 if (seleccion==3){
-                    System.out.println("la division"+(num1/num2));
+                    System.out.println("la division"+(num1 /num2));
                 }else{
                     System.out.println("opcion no valida");
                 }
