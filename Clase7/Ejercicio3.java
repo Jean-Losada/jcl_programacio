27eb3bc3907a10b8124e8c588bfc830e65ce2781
Jean-Losada <jclosada@estudiante.uniajc.edu.co>
2026-03-16 17:16:05 +0000

    ok

--- Clase7/Ejercicio3.java
+++ Clase7/Ejercicio3.java
@@ -1,5 +1,24 @@
 package Clase7;
 
+import java.util.Scanner;
+
 public class Ejercicio3 {
-    
-}
+    public static void main(String[] args) {
+        
+        Scanner sc = new Scanner(System.in);
+        int num;
+        int suma = 0;
+
+        System.out.println("Ingrese numeros (0 para terminar): ");
+        num = sc.nextInt();
+
+        while(num != 0){
+            suma = suma + num;
+            num = sc.nextInt();
+
+            System.out.println("La suma total es: " + suma);
+            
+            
+        }
+    }
+  }
